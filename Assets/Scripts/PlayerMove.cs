65c2fd68cbb574c1c3c11f526fc9514d93d22da5
안성범 <seongbeom021127@naver.com>
2025-09-30 17:21:00 +0900

    animation and script changde

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,10 +6,14 @@ public class PlayerMove : MonoBehaviour
 {
     public float MaxSpeed;
     Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
     // Start is called before the first frame update
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
     void Update()
     {
@@ -19,6 +23,19 @@ public class PlayerMove : MonoBehaviour
             
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
         }
+        //Rirection Sprite
+        if (Input.GetButton("Horizontal"))
+        {
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        }
+        //Animation
+        if (Mathf.Abs(rigid.velocity.x) < 0.3)
+        {
+            anim.SetBool("isWalking",false);
+        }
+        else
+            anim.SetBool("isWalking", true);
+
     }
     // Update is called once per frame
     void FixedUpdate()
